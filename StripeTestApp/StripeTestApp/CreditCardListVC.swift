d01856183349b2a1a103240c72d97b2fbd5cf447
Sergey <pogrebnyak.vetal1996@gmail.com>
2022-11-28 11:55:41 +0200

    start implement logic to delete payment method

--- StripeTestApp/StripeTestApp/CreditCardListVC.swift
+++ StripeTestApp/StripeTestApp/CreditCardListVC.swift
@@ -13,12 +13,7 @@ final class CreditCardListVC: UIViewController {
     @IBOutlet private weak var paymentMethodsTableView: UITableView!
     
     // MARK: Variables
-    private var creditCards = [SavedCard]() {
-        didSet {
-            refreshControl.endRefreshing()
-            paymentMethodsTableView.reloadData()
-        }
-    }
+    private var creditCards = [SavedCard]()
     
     private lazy var refreshControl: UIRefreshControl = {
         let view = UIRefreshControl()
@@ -54,16 +49,49 @@ final class CreditCardListVC: UIViewController {
         paymentMethodsTableView.refreshControl = refreshControl
     }
     
+    private func endRefreshingAndReloadTableView() {
+        refreshControl.endRefreshing()
+        paymentMethodsTableView.reloadData()
+    }
+    
     // MARK: Other
     private func fetchSavedCreditCards() {
         ServerNetworkManager().getSavedCC() { [weak self] in
             switch $0 {
             case .success(let creditCards):
                 self?.creditCards = creditCards
+                self?.endRefreshingAndReloadTableView()
             case .failure(let error):
                 ProgressHUD.show(error: error.localizedDescription)
             }
         }
+    }
+    
+    private func showAreYouSureAlert(toConfirmDeleteAt indexPath: IndexPath, trailingActionCallback: @escaping (Bool) -> Void) {
+        let yesAction: UIAlertAction = .init(withTitle: R.string.localizable.yes(), style: .destructive) { [weak self] _ in
+            self?.deleteCC(at: indexPath, withTrailingActionCallback: trailingActionCallback)
+        }
+        
+        let noAction: UIAlertAction = .init(withTitle: R.string.localizable.no()) { _ in
+            trailingActionCallback(false)
+        }
+        
+        showAlert(withTitle: R.string.localizable.ccListAreYouSure(),
+                  message: R.string.localizable.ccListConfirmToDeleteAction(),
+                  actions: [yesAction, noAction])
+    }
+    
+    private func deleteCC(at indexPath: IndexPath, withTrailingActionCallback callback: @escaping (Bool) -> Void) {
+        ProgressHUD.show()
+//        success flow
+//        ProgressHUD.hide()
+//        creditCards.remove(at: indexPath.row)
+//        paymentMethodsTableView.deleteRows(at: [indexPath], with: .automatic)
+//        callback(true)
+        
+//        failure flow
+//        ProgressHUD.hide()
+//        callback(false)
     }
 }
 
@@ -78,5 +106,17 @@ extension CreditCardListVC: UITableViewDataSource {
         let cell = UITableViewCell()
         cell.textLabel?.text = creditCards[indexPath.row].description
         return cell
+    }
+}
+
+extension CreditCardListVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: R.string.localizable.delete()) { [weak self] (_, _, completionHandler) in
+            self?.showAreYouSureAlert(toConfirmDeleteAt: indexPath,
+                                      trailingActionCallback: completionHandler)
+        }
+        
+        return .init(actions: [deleteAction])
     }
 }
