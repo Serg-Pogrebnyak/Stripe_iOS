80a17839d7b8c8aa43e0384dc157f7de9b4315cc
Sergey <pogrebnyak.vetal1996@gmail.com>
2022-12-05 17:39:31 +0200

    small changes in configuration of progress hud

--- StripeTestApp/StripeTestApp/ProgressHUD.swift
+++ StripeTestApp/StripeTestApp/ProgressHUD.swift
@@ -10,28 +10,36 @@ import SVProgressHUD
 struct ProgressHUD {
     
     static func show() {
-        SVProgressHUD.setDefaultMaskType(.clear)
-        SVProgressHUD.setDefaultAnimationType(.native)
+        configureProgressHud()
         DispatchQueue.main.async {
             SVProgressHUD.show()
         }
     }
     
     static func hide() {
+        configureProgressHud()
         DispatchQueue.main.async {
             SVProgressHUD.dismiss()
         }
     }
     
     static func show(success: String) {
+        configureProgressHud()
         DispatchQueue.main.async {
             SVProgressHUD.showSuccess(withStatus: success)
         }
     }
     
     static func show(error: String) {
+        configureProgressHud()
         DispatchQueue.main.async {
             SVProgressHUD.showError(withStatus: error)
         }
+    }
+    
+    private static func configureProgressHud() {
+        SVProgressHUD.setDefaultMaskType(.clear)
+        SVProgressHUD.setDefaultAnimationType(.native)
+        SVProgressHUD.setMaximumDismissTimeInterval(2)
     }
 }
