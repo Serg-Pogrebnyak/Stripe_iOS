e07e2d773c1173896d5c52561545f8390f7bbcff
Sergey <pogrebnyak.vetal1996@gmail.com>
2022-12-07 11:11:53 +0200

    refactoring

--- StripeTestApp/StripeTestApp/CreateNewPaymentMethodVC.swift
+++ StripeTestApp/StripeTestApp/CreateNewPaymentMethodVC.swift
@@ -30,19 +30,19 @@ final class CreateNewPaymentMethodVC: UIViewController {
     // MARK: UI
     private func setupUI() {
         creditCardTextField.postalCodeEntryEnabled = false
-        createAndAttachPaymentButton.setTitle("Create & Attach payment method", for: .normal)
+        createAndAttachPaymentButton.setTitle(R.string.localizable.createNewPMCreateAndAttach(), for: .normal)
     }
     
     // MARK: Other
     private func createPaymentMethod() {
-        guard creditCardTextField.isValid else { return ProgressHUD.show(error: "Credit card data is invalid") }
+        guard creditCardTextField.isValid else { return ProgressHUD.show(error: R.string.localizable.createNewPMCardIsInvalid()) }
         
         ProgressHUD.show()
         STPAPIClient.shared.createPaymentMethod(with: creditCardTextField.paymentMethodParams) { [weak self] paymentMethod, error in
             if let paymentMethodUnwrapped = paymentMethod {
                 self?.attachPaymentMethodToCustomer(paymentMethodUnwrapped.stripeId)
             } else {
-                ProgressHUD.show(error: error?.localizedDescription ?? "no error description")
+                ProgressHUD.show(error: error?.localizedDescription ?? R.string.localizable.createNewPMSomeError())
             }
         }
     }
@@ -51,7 +51,7 @@ final class CreateNewPaymentMethodVC: UIViewController {
         ServerNetworkManager().attach(paymentMethodId: paymentId) {
             switch $0 {
             case .success(let response):
-                ProgressHUD.show(success: "Payment method attached to customer with id: \(response.customerId), card info: \(response.creditCard.description)")
+                ProgressHUD.show(success: response.description)
             case .failure(let error):
                 ProgressHUD.show(error: error.localizedDescription)
             }
@@ -63,5 +63,14 @@ final class CreateNewPaymentMethodVC: UIViewController {
 extension CreateNewPaymentMethodVC: STPAuthenticationContext {
     func authenticationPresentingViewController() -> UIViewController {
         self
+    }
+}
+
+fileprivate extension AttachPaymentMethodResponse {
+    var description: String {
+        [
+            R.string.localizable.createNewPMAttachedTo() + customerId,
+            R.string.localizable.createNewPMCardInfo() + creditCard.description
+        ].joined(separator: ",")
     }
 }
